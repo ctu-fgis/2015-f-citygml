110967f8325eaf0255cc4cbad7605858a05c4a76
Miro Hrončok <miro@hroncok.cz>
2015-03-26 15:08:36 +0100

    Add basic Python library file structure
     - using DevAssistant

---
+++ setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(
+    name='citygml2stl',
+    version='0.1',
+    description='CityGML to printable STL',
+    long_description=''.join(open('README.rst').readlines()),
+    keywords='3D printing, STL, mesh, CityGML',
+    author='Miro Hrončok',
+    author_email='miro@hroncok.cz',
+    license='MIT',
+    packages=find_packages(),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Manufacturing',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ]
+)
