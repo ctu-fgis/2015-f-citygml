8684ec06d37c3f04e214c87cf7e03c99b5542379
Miro Hrončok <miro@hroncok.cz>
2015-06-16 17:49:13 +0200

    Download the datasets when running the tests

--- test/__init__.py
+++ test/__init__.py
@@ -1,12 +1,39 @@
 import os
 import sys
+import zipfile
+try:
+    from urllib import urlretrieve
+except ImportError:
+    from urllib.request import urlretrieve
+
+
+BASEURL = 'http://www.citygml.org/fileadmin/count.php?f=fileadmin/citygml/docs/'
+FILES = [
+    'geoRES_testdata_v1.0.0',
+    'waldbruecke_v1.0.0',
+    'CityGML_2.0_Test_Dataset_2012-04-23',
+    'Berlin_Alexanderplatz_v0.4.0',
+]
 
 abspath = os.path.abspath(__file__)
-testdir = os.path.dirname(abspath)
-projdir = os.path.dirname(testdir)
-sys.path.insert(0, projdir)
+dname = os.path.dirname(abspath)
+pwd = os.getcwd()
+os.chdir(os.path.join(dname, 'datasets'))
 
-if not os.path.exists('test/datasets/geoRES_testdata_v1.0.0'):
-    sys.stderr.write('You don\'t have files neccessary for testing\n')
-    sys.stderr.write('Please read test/datasets/README.rst\n')
-    exit(1)
+for filename in FILES:
+    if not os.path.exists(filename) and not os.path.exists(filename + '.gml') and not os.path.exists(filename + '.xml'):
+        if not os.path.exists(filename + '.zip'):
+            print('Downloading {}'.format(filename))
+            if filename == 'CityGML_2.0_Test_Dataset_2012-04-23':
+                url = 'http://dl.dropbox.com/u/24313387/CityGML_2.0_Test_Dataset_2012-04-23.zip'
+            else:
+                url = BASEURL + filename + '.zip'
+            urlretrieve(url, filename + '.zip')
+        print('Extracting {}'.format(filename))
+        with zipfile.ZipFile(filename + '.zip', 'r') as zip:
+            zip.extractall('.')
+
+os.chdir(pwd)
+
+projdir = os.path.dirname(dname)
+sys.path.insert(0, projdir)
--- test/datasets/README.rst
+++ test/datasets/README.rst
@@ -5,5 +5,10 @@ This folder should contain CityGML datasets for testing purposes.
 Unfortunately, datasets from `CityGML website <http://www.citygml.org/index.php?id=1539>`_
 cannot be freely redistributed.
 
-In order to obtain the datasets, run ``download.py`` from this directory.
+When the test are invoked for the first time, the files should be downloaded.
 
+Terms for using those files
+---------------------------
+
+All datasets can be downloaded freely, but may be used for evaluation, research, and education
+purposes only. Please note the `respective copyrights <http://www.citygml.org/index.php?id=1539>`_.
